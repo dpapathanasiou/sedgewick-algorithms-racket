01174c6fe5b7caf070dce455c10575631593e1ac
Denis Papathanasiou <denis@papathanasiou.org>
2019-06-08 18:31:58 -0400

    Helper methods to fetch the results (valid indices of the resulting vector)

--- chapter-3/eratosthenes.rkt
+++ chapter-3/eratosthenes.rkt
@@ -1,5 +1,15 @@
 #lang racket/base
 
+(provide primes)
+
+(define (not-void? x)
+  (not (void? x)))
+
+(define (get-indices v)
+  (for/list ([i (in-range 2 (vector-length v))])
+    (when (= (vector-ref v i) 1)
+      i)))
+
 (define (sieve n)
   (let ((arr (make-vector n 1)))
     (for ([i (in-range 2 n)])
@@ -7,6 +17,8 @@
         (vector-set! arr (* i j) 0)))
     arr))
 
+(define (primes n)
+  (filter not-void? (get-indices (sieve n))))
 
  
 
