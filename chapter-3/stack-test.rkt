d4939e740e1e20550cea875dc1be84ad234c7df0
Denis Papathanasiou <denis@papathanasiou.org>
2019-07-07 11:09:37 -0400

    Added a second LIFO test, on pop

--- chapter-3/stack-test.rkt
+++ chapter-3/stack-test.rkt
@@ -4,8 +4,16 @@
          "stack.rkt")
 
 (test-case
-  "Confirm LIFO sequence"
-  (let ([stack (make-stack)])
-    (for ([x (list 1 2 3 4)])
-      (set! stack (push x stack)))
-    (check-equal? stack (list 4 3 2 1))))
+ "Confirm LIFO sequence on push"
+ (let ([stack (make-stack)])
+   (for ([x (list 1 2 3 4)])
+     (set! stack (push x stack)))
+   (check-equal? stack (list 4 3 2 1))))
+
+(test-case
+ "Confirm LIFO sequence on pop"
+ (let ([stack (make-stack)])
+   (for ([x (list 1 2 3 4)])
+     (set! stack (push x stack)))
+   (let-values ([(head tail) (pop stack)])
+     (check-equal? head 4))))
