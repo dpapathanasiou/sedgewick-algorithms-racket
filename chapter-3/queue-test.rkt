0897ad2759be568a96f9ed1ab88dd85b8ee38f9f
Denis Papathanasiou <denis@papathanasiou.org>
2019-07-14 15:38:29 -0400

    How the rest of the queue is stored is irrelevant, so long as the FIFO on dequeue works

--- chapter-3/queue-test.rkt
+++ chapter-3/queue-test.rkt
@@ -4,10 +4,9 @@
          "queue.rkt")
 
 (test-case
- "Confirm FIFO sequence on pop"
+ "Confirm FIFO sequence on dequeue"
  (let ([queue (make-queue)])
    (for ([x (list 1 2 3 4)])
      (set! queue (enqueue x queue)))
    (let-values ([(head tail) (dequeue queue)])
-     (check-equal? head 1)
-     (check-equal? tail '(4 3 2)))))
+     (check-equal? head 1))))
