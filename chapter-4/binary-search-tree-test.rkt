04b6e471da4ee63ca8a858a29510b10801bfebab
Denis Papathanasiou <denis@papathanasiou.org>
2019-10-06 19:24:01 -0400

    Implemented traversal methods for pre/post/in-order traversal, along with associated tests

--- chapter-4/binary-search-tree-test.rkt
+++ chapter-4/binary-search-tree-test.rkt
@@ -9,3 +9,29 @@
 
 (check-equal? (list->tree (string->integer-list "search"))
               (node 115 (node 101 (node 97 '() (node 99 '() '())) (node 114 (node 104 '() '()) '())) '()))
+
+; Traversal tests, given an example tree of:
+;
+;     1
+;    / \
+;   2   3
+;  / \
+; 4   5
+
+(define example-tree
+  (node 1 (node 2 (node 4 '() '()) (node 5 '() '())) (node 3 '() '())))
+
+(define (extract-values nodes-list)
+  (map (λ(x) (node-value x)) nodes-list))
+
+(check-equal? (extract-values
+               (inorder-traversal example-tree))
+              '(4 2 5 1 3))
+
+(check-equal? (extract-values
+               (preorder-traversal example-tree))
+              '(1 2 4 5 3))
+
+(check-equal? (extract-values
+               (postorder-traversal example-tree))
+              '(4 5 2 3 1))
--- chapter-4/binary-search-tree.rkt
+++ chapter-4/binary-search-tree.rkt
@@ -1,6 +1,11 @@
 #lang racket/base
 
-(provide node list->tree)
+(provide node
+         node-value
+         list->tree
+         inorder-traversal
+         preorder-traversal
+         postorder-traversal)
 
 (struct node (value left right)
   #:transparent
@@ -32,4 +37,29 @@
         (l->t (cdr l) (insert a (car l)))))
   (l->t lst '()))
 
-; TODO: implement (traverse) for pre/post/in-order traversal
+(define (inorder-traversal tree)
+  (let ([nodes '()])
+    (unless (null? (node-left tree))
+      (set! nodes (append nodes (inorder-traversal (node-left tree)))))
+    (set! nodes (append nodes (list tree)))
+    (unless (null? (node-right tree))
+      (set! nodes (append nodes (inorder-traversal (node-right tree)))))
+    nodes))
+
+(define (preorder-traversal tree)
+  (let ([nodes '()])
+    (set! nodes (append nodes (list tree)))
+    (unless (null? (node-left tree))
+      (set! nodes (append nodes (preorder-traversal (node-left tree)))))
+    (unless (null? (node-right tree))
+      (set! nodes (append nodes (preorder-traversal (node-right tree)))))
+    nodes))
+
+(define (postorder-traversal tree)
+  (let ([nodes '()])
+    (unless (null? (node-left tree))
+      (set! nodes (append nodes (postorder-traversal (node-left tree)))))
+    (unless (null? (node-right tree))
+      (set! nodes (append nodes (postorder-traversal (node-right tree)))))
+    (set! nodes (append nodes (list tree)))
+    nodes))
