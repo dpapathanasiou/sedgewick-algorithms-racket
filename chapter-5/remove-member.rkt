ca65bc3728bd80f1c9f0b609cebb849ade2e4419
Denis Papathanasiou <denis@papathanasiou.org>
2020-08-23 15:20:21 -0400

    Simplified the definition of (remove-member) using (if) instead of (cond)

--- chapter-5/remove-member.rkt
+++ chapter-5/remove-member.rkt
@@ -4,10 +4,6 @@
 
 (define remove-member
   (λ (a lat)
-    (cond
-      ((null? lat) '())
-      (else
-       (cond
-         ((eq? (car lat) a) (cdr lat))
-         (else
-          (cons (car lat) (remove-member a (cdr lat)))))))))
+    (if (null? lat) '()
+        (if (eq? (car lat) a) (cdr lat)
+            (cons (car lat) (remove-member a (cdr lat)))))))
